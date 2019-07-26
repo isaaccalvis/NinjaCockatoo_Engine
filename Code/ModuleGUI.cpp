fe5c4c418447b860001a92cc938583e90f433032
isaaccalvis <isaaccalvis@gmail.com>
2019-07-26 18:06:58 +0200

    ImGui integrated & SDL_Mixer deleted

--- Code/ModuleGUI.cpp
+++ Code/ModuleGUI.cpp
@@ -1,7 +1,39 @@
 #include "Application.h"
 #include "ModuleGUI.h"
 
+#include "imgui\imgui.h"
+#include "imgui\imgui_impl_opengl2.h"
+#include "imgui\imgui_impl_sdl.h"
+
 ModuleGUI::ModuleGUI(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 
+}
+
+bool ModuleGUI::Init()
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+	ImGui_ImplOpenGL2_Init();
+
+	return true;
+}
+
+update_status ModuleGUI::Update(float dt)
+{
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame(App->window->window);
+	ImGui::NewFrame();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+	return UPDATE_CONTINUE;
 }
--- Code/ModuleGUI.h
+++ Code/ModuleGUI.h
@@ -7,6 +7,10 @@ class ModuleGUI : public Module
 {
 public:
 	ModuleGUI(Application* app, bool start_enabled = true);
+
+	bool Init();
+	update_status Update(float dt);
+
 };
 
 #endif
