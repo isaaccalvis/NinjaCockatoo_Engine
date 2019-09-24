a309a92aca9f5aa766f9af2cc84c46116c57cba4
AitorVelez <aitorvelez@hotmail.com>
2019-09-24 17:40:26 +0200

    Corrected Mesh Frustrum Vertices

--- Code/MeshFrustrum.cpp
+++ Code/MeshFrustrum.cpp
@@ -4,14 +4,14 @@ MeshFrustrum::MeshFrustrum() : Mesh()
 {
 	verticesSize = 24;
 	verticesArray = new GLfloat[verticesSize]{
-		0.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.25f, 0.25f, 1.0f,
-		0.75f, 0.25f, 1.0f,
-		0.75f, 0.75f, 1.0f,
-		0.25f, 0.75f, 1.0f
+		0.0f,0.0f, 0.0f,
+		1.0f,0.0f, 0.0f,
+		1.0f,0.0f, 1.0f,
+		0.0f,0.0f, 1.0f,
+		0.25f, 1.0f, 0.25f,
+		0.75f, 1.0f, 0.25f,
+		0.75f, 1.0f, 0.75f,
+		0.25f, 1.0f, 0.75f
 	};
 
 	vertices = 0u;
