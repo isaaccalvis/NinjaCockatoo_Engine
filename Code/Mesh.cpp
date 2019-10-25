b99f46aac43847c942b1419653e11c0b33c52e0c
isaaccalvis <isaaccalvis@gmail.com>
2019-10-25 22:33:38 +0200

    Now to add primitives we use par_shapes.h (external)

--- Code/Mesh.cpp
+++ Code/Mesh.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Mesh.h"
+#include "Application.h"
 
 Mesh::Mesh()
 {}
@@ -25,22 +26,51 @@ void Mesh::Render(Texture* texture)
 {
 	glPushMatrix();
 
+	// Draw Geometry
+	if (texture != nullptr)
+	{
+		glBindTexture(GL_TEXTURE_2D, texture->GetBufferPos());
+	}
 	glEnableClientState(GL_VERTEX_ARRAY);
 
 	glTranslatef(position.x, position.y, position.z);
 	glRotated(rotation.Angle() * RADTODEG, rotation.x, rotation.y, rotation.z);
-	glScaled(scale.x, scale.y, scale.z);
+	glScalef(scale.x, scale.y, scale.z);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vertices);
-	glVertexPointer(3, GL_FLOAT, 0, nullptr);
+	glVertexPointer(3, GL_FLOAT, 0, NULL);
+
+	if (textureCoords != nullptr)
+	{
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glBindBuffer(GL_ARRAY_BUFFER, textureIndex);
+		glTexCoordPointer(2, GL_FLOAT, 0, NULL);
+	}
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices);
-	glDrawElements(GL_TRIANGLES, indicesSize, GL_UNSIGNED_INT, nullptr);
-
+	glDrawElements(GL_TRIANGLES, indicesSize, GL_UNSIGNED_INT, NULL);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 	glDisableClientState(GL_VERTEX_ARRAY);
+
+	// Draw Normals
+	if (App->renderer3D->renderNormals)
+	{
+		for (int i = 0; i < verticesSize; i++)
+		{
+			if (normals != nullptr)
+				normals[i].Render();
+		}
+	}
+
+	if (App->renderer3D->renderBoudingBox)
+	{
+		if (boundingBoxCube != nullptr)
+			boundingBoxCube->Render();
+	}
 
 	glPopMatrix();
 }
--- Code/Primitive.cpp
+++ Code/Primitive.cpp
@@ -1,27 +1,37 @@
 #include "Primitive.h"
+#include "par_shapes.h"
 
 MeshPrimitive::MeshPrimitive() : Mesh()
 {}
 
 MeshPrimitive::MeshPrimitive(PRIMITIVE_MESHES type) : Mesh()
 {
+
 	verticesSize = 0;
 	indicesSize = 0;
+	par_shapes_mesh* primitive;
 	switch (type)
 	{
 	case PRIMITIVE_MESHES::PRIMITIVE_CUBE:
+		primitive = par_shapes_create_cube();
+		break;
+	case PRIMITIVE_MESHES::PRIMITIVE_PLANE:
+		primitive = par_shapes_create_plane(1, 1);
+		break;
+	case PRIMITIVE_MESHES::PRIMITIVE_FRUSTRUM:
 	{
 		verticesSize = 24;
 		verticesArray = new GLfloat[verticesSize]{
 			-0.5f, -0.5f, -0.5f,
 			 0.5f, -0.5f, -0.5f,
-			 0.5f,  0.5f, -0.5f,
-			-0.5f,  0.5f, -0.5f,
-			-0.5f, -0.5f,  0.5f,
 			 0.5f, -0.5f,  0.5f,
-			 0.5f,  0.5f,  0.5f,
-			-0.5f,  0.5f,  0.5f
+			-0.5f, -0.5f,  0.5f,
+			-0.25f, 0.5f, -0.25f,
+			 0.25f, 0.5f, -0.25f,
+			 0.25f, 0.5f,  0.25f,
+			-0.25f, 0.5f,  0.25f
 		};
+
 		indicesSize = 36;
 		indicesArray = new unsigned int[indicesSize] {
 			2, 1, 0,
@@ -39,94 +49,30 @@ MeshPrimitive::MeshPrimitive(PRIMITIVE_MESHES type) : Mesh()
 		};
 	}
 	break;
-	case PRIMITIVE_MESHES::PRIMITIVE_PLANE:
-	{
-		verticesSize = 12;
-		verticesArray = new GLfloat[verticesSize]{
-			-0.5f,  -0.5f, -0.5f,
-			 0.5f,  -0.5f, -0.5f,
-			-0.5f,  -0.5f,  0.5f,
-			 0.5f,  -0.5f,  0.5f
-		};
-
-		indicesSize = 6;
-		indicesArray = new unsigned int[indicesSize] {
-			0, 2, 1,
-				2, 3, 1
-		};
-	}
-	break;
-	case PRIMITIVE_MESHES::PRIMITIVE_FRUSTRUM:
-	{
-		verticesSize = 24;
-		verticesArray = new GLfloat[verticesSize]{
-			-0.5f, -0.5f, -0.5f,
-			 0.5f, -0.5f, -0.5f,
-			 0.5f, -0.5f,  0.5f,
-			-0.5f, -0.5f,  0.5f,
-			-0.25f, 0.5f, -0.25f,
-			 0.25f, 0.5f, -0.25f,
-			 0.25f, 0.5f,  0.25f,
-			-0.25f, 0.5f,  0.25f
-		};
-
-		indicesSize = 36;
-		indicesArray = new unsigned int[indicesSize] {
-				2, 1, 0,
-				3, 2, 0,
-				2, 5, 1,
-				5, 2, 6,
-				3, 7, 2,
-				2, 7, 6,
-				5, 6, 4,
-				6, 7, 4,
-				3, 0, 4,
-				7, 3, 4,
-				0, 1, 4,
-				1, 5, 4
-		};
-	}
-	break;
 	case PRIMITIVE_MESHES::PRIMITIVE_CYLINDER:
-	{
-		verticesSize = 24;
-		verticesArray = new GLfloat[verticesSize];
-
-		float x;
-		float y;
-		float angle;
-
-		for (angle = 0.0; angle < 2 * pi; angle++) {
-			*verticesArray++ = cos(angle);
-			*verticesArray++ = sin(angle);
-			*verticesArray++ = 1;
-		}
-
-		indicesSize = 36;
-		indicesArray = new unsigned int[indicesSize] {
-				2, 1, 0,
-				3, 2, 0,
-				2, 5, 1,
-				5, 2, 6,
-				3, 7, 2,
-				2, 7, 6,
-				5, 6, 4,
-				6, 7, 4,
-				3, 0, 4,
-				7, 3, 4,
-				0, 1, 4,
-				1, 5, 4
-		};
-	}
-	break;
+		primitive = par_shapes_create_cylinder(16, 8);
+		break;
 	case PRIMITIVE_MESHES::PRIMITIVE_SPHERE:
-	{
-
-	}
-	break;
-	default: {}
+		primitive = par_shapes_create_subdivided_sphere(3);
 		break;
 	}
+
+	verticesSize = primitive->npoints * 3;
+	verticesArray = new GLfloat[verticesSize];
+	for (int i = 0; i < verticesSize; i++)
+	{
+		verticesArray[i] = primitive->points[i];
+	}
+
+	indicesSize = primitive->ntriangles * 3;
+	indicesArray = new unsigned int[indicesSize];
+	for (int i = 0; i < indicesSize; i++)
+	{
+		indicesArray[i] = primitive->triangles[i];
+	}
+
+	textureCoords = new GLfloat[primitive->npoints * 2];
+	textureCoords = primitive->tcoords;
 
 	vertices = 0u;
 	glGenBuffers(1, (GLuint*) &(vertices));
@@ -139,8 +85,12 @@ MeshPrimitive::MeshPrimitive(PRIMITIVE_MESHES type) : Mesh()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indicesSize, indicesArray, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-}
 
+	// Bounding Box
+	//boundingBox.SetNegativeInfinity();
+	//boundingBox.Enclose((const math::float3*)verticesArray, verticesSize);
+	//boundingBoxCube = new DebugCube(boundingBox.CenterPoint(), boundingBox.Size());
+}
 
 PRIMITIVE_MESHES MeshPrimitive::GetType() const
 {
