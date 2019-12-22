aa2ce62ea2d7b978c6fd0a460793ff612dc7f8a1
isaaccalvis <isaaccalvis@gmail.com>
2019-12-22 15:18:22 +0100

    Readapting Physics panel

--- Code/GUI_Physics.cpp
+++ Code/GUI_Physics.cpp
@@ -12,11 +12,11 @@ void GUI_Physics::Draw()
 {
 	ImGui::Begin("Physics", &active, ImGuiWindowFlags_NoFocusOnAppearing);
 	ImGui::SetWindowPos(ImVec2(App->window->screenWidth / 5, App->window->screenHeight*.07 + 18.0f), ImGuiCond_Once);
-	ImGui::SetWindowSize(ImVec2(App->window->screenWidth*0.6f, App->window->screenHeight*.07), ImGuiCond_Once);
+	ImGui::SetWindowSize(ImVec2(App->window->screenWidth*0.6f, App->window->screenHeight*.07 * 1.8f), ImGuiCond_Once);
 	if (App->gui->automaticGUI_pos)
 	{
 		ImGui::SetWindowPos(ImVec2(App->window->screenWidth / 5, App->window->screenHeight*.07 + 18.0f));
-		ImGui::SetWindowSize(ImVec2(App->window->screenWidth*0.6f, App->window->screenHeight*.07));
+		ImGui::SetWindowSize(ImVec2(App->window->screenWidth*0.6f, App->window->screenHeight*.07 * 1.8f));
 	}
 
 	ImGui::Checkbox("Activate Physics", &App->physics->activePhysics);
